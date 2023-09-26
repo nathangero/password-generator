dcb6304413fdd09fd2890fa53141be77a8842d1c
nathangero <nathangero@yahoo.com>
2023-09-26 16:06:15 -0700

    implement prompting user for password length

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var passwordCriteria = {
+    lowercase: false,
+    uppercase: false,
+    numeric: false,
+    specialChars: false,
+  }
+
+  var passwordLength = prompt("How many characters do you want your password to have?");
+
+  // Check if password length is long enough.
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must between 8-128 characters. Please try again.");
+  } else if (isNaN(passwordLength)) {
+    alert("Please enter a number.");
+  }
+
+  return "";
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
